8a23cc91a20112abe73eee868c9c47dd181794be
SergioFalcone1 <sergio.falcone1@hotmail.it>
2025-12-03 10:52:30 -0500

    Add files via upload

---
+++ UNIT_1/S2/L3/01_pratica.py
@@ -0,0 +1,7 @@
+#GENERATORE DI NOME PER UNA BAND
+
+Città = input('inseririre il nome della città di origine: ')
+Animale = input('inserire il nome del proprio animale domestico: ')
+
+nomeband = Città + ' '+ Animale
+print('il nome della band è: ',nomeband)
